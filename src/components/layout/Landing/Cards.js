52aaee0ccbbeee78bbcf21b57456ac2e918d1415
ZainHasan97-Razi <zainrazi97@yahoo.com>
2020-09-04 16:16:40 +0500

    changes

--- src/components/layout/Landing/Cards.js
+++ src/components/layout/Landing/Cards.js
@@ -5,6 +5,7 @@ import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import FacebookIcon from "@material-ui/icons/Facebook";
+import "./index.css";
 
 const useStyles = makeStyles({
   root: {
@@ -32,9 +33,30 @@ const Cards = () => {
   const classes = useStyles();
 
   return (
-    <div className="card-wrapper">
+    <div
+      className="card-wrapper"
+      // style={{
+      //   display: "flex",
+      //   alignSelf: "center",
+      //   // backgroundColor: 'red',
+      //   width: "60%",
+      //   marginTop: "-150px",
+      // }}
+    >
       <Card className={classes.root}>
-        <CardContent className="card-content1">
+        <CardContent
+          style={{
+            display: "flex",
+            flex: 1,
+            backgroundColor: "#ffffff",
+
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            paddingBottom: 100,
+            paddingTop: 100,
+          }}
+        >
           <Typography variant="h3">12,154</Typography>
           <Typography
             variant="body1"
@@ -42,27 +64,74 @@ const Cards = () => {
           >
             People have already joined, jump in !
           </Typography>
-          <Button variant="contained" color="primary" className="sign-fb-btn">
+          <Button
+            variant="contained"
+            color="primary"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              backgroundColor: "#7348EF",
+              textTransform: "none",
+              marginTop: "20px",
+            }}
+          >
             <FacebookIcon style={{ marginRight: 10 }} /> Sign in via Facebook
           </Button>
-          <Typography variant="caption" className="text-youre">
+          <Typography
+            variant="caption"
+            style={{ width: 200, textAlign: "center", marginTop: "20px" }}
+          >
             We never post on your behalf
           </Typography>
         </CardContent>
-        <CardContent className="card-content2">
+        <CardContent
+          style={{
+            display: "flex",
+            flex: 1,
+            backgroundColor: "#F6F6F6",
+
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+            borderRadius: "30px",
+          }}
+        >
           <Typography
             variant="h6"
             style={{ width: 200, textAlign: "center", marginTop: "40px" }}
           >
             Sign up by answering a few questions:
           </Typography>
-          <Typography variant="body1" className="text-youre">
+          <Typography
+            variant="body1"
+            style={{ width: 200, textAlign: "center", marginTop: "20px" }}
+          >
             You're
           </Typography>
-          <Button variant="contained" className="btn-malefemale">
+          <Button
+            variant="contained"
+            //color='#ffffff'
+
+            style={{
+              textTransform: "none",
+              width: "80%",
+              marginTop: "20px",
+              borderRadius: "7px",
+              backgroundColor: "#ffffff",
+            }}
+          >
             Male
           </Button>
-          <Button variant="contained" className="btn-malefemale">
+          <Button
+            variant="contained"
+            style={{
+              textTransform: "none",
+              width: "80%",
+              marginTop: "20px",
+              borderRadius: "7px",
+              backgroundColor: "#ffffff",
+            }}
+          >
             Female
           </Button>
         </CardContent>
