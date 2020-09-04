6a9268076485d520d0e66833e740235ca8669f16
talalabbas84@gmail.com <talalabbas84@gmail.com>
2020-09-04 18:11:08 +0500

    Responsiveness of landing page

--- src/components/layout/Landing/Header.js
+++ src/components/layout/Landing/Header.js
@@ -1,45 +1,45 @@
-import React from "react";
-import FavoriteBorderOutlinedIcon from "@material-ui/icons/FavoriteBorderOutlined";
-import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
-import hiverLogo from "../../../asset/images/hiver-logo.png";
-import { Button } from "@material-ui/core";
+import React from 'react';
+import FavoriteBorderOutlinedIcon from '@material-ui/icons/FavoriteBorderOutlined';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import hiverLogo from '../../../asset/images/hiver-logo.png';
+import { Button } from '@material-ui/core';
 
 const Header = () => {
   return (
-    <div class="d-flex justify-content-around">
-      <div className="div-main">
-        <img style={{ width: "130px", height: "84px" }} src={hiverLogo} />
+    <div class='d-flex justify-content-around div-main-main-wrapper'>
+      <div className='div-main'>
+        <img style={{ width: '130px', height: '84px' }} src={hiverLogo} />
       </div>
-      <div className="div-right">
-        <div className="div-encounter">
+      <div className='div-right'>
+        <div className='div-encounter'>
           <FavoriteBorderOutlinedIcon
-            style={{ color: "#fff" }}
-            fontSize={"large"}
+            style={{ color: '#fff' }}
+            fontSize={'large'}
           />
           <p
             style={{
-              margin: "2px",
-              color: "white",
+              margin: '2px',
+              color: 'white'
             }}
           >
             Encounter
           </p>
         </div>
-        <div className="div-nearby">
+        <div className='div-nearby'>
           <LocationOnOutlinedIcon
-            style={{ color: "#fff" }}
-            fontSize={"large"}
+            style={{ color: '#fff' }}
+            fontSize={'large'}
           />
           <p
             style={{
-              margin: "2px",
-              color: "white",
+              margin: '2px',
+              color: 'white'
             }}
           >
             People nearby
           </p>
         </div>
-        <Button variant="outlined" size="small" className="button-signin">
+        <Button variant='outlined' size='small' className='button-signin'>
           Sign In
         </Button>
       </div>
