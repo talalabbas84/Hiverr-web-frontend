e6575a18217ca425a2ba19d61d78fa795832d036
talalabbas84@gmail.com <talalabbas84@gmail.com>
2020-09-07 18:02:10 +0500

    Fixed the responsivesness

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
@@ -7,9 +8,9 @@ import './App.css';
 
 import { Navbar } from './components/layout/Navbar';
 import Landing from './components/layout/Landing/Landing';
+import SignIn from './components/layout/SignIn/SignIn';
 
 const App = () => {
-  console.log(Navbar);
   return (
     <Fragment>
       <Landing />
---
+++ src/components/layout/SignIn/SignIn.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Header from '../Header/Header';
+
+const SignIn = () => {
+  return <Header />;
+};
+
+export default SignIn;
