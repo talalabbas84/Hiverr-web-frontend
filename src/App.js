45b7cfa79faee721efe5de5eb264b655ebf67c68
talalabbas84@gmail.com <talalabbas84@gmail.com>
2020-09-08 22:48:41 +0500

    testing the application

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './App.css';
 
-import { Navbar } from './components/layout/Navbar';
 import Landing from './components/layout/Landing/Landing';
 import SignIn from './components/layout/SignIn/SignIn';
 
