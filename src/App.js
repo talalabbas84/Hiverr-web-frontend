e9cbc89d5b188446caaee8a9dcdd3cd15094f185
talalabbas84@gmail.com <talalabbas84@gmail.com>
2020-09-10 16:06:38 +0500

    Redux setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import { Provider } from 'react-redux';
 import './App.css';
 
 import Landing from './components/layout/Landing/Landing';
@@ -11,19 +12,26 @@ import SignIn from './components/layout/SignIn/SignIn';
 import { Navbar } from './components/layout/Navbar';
 import Encounter from './components/layout/Encounter';
 import SignUp from './components/layout/SignUp/SignUp';
+import store from './store';
+import { loadUser } from './actions/auth';
 
 const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
   return (
-    <Fragment>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/login' component={SignIn} /> */}
-          <Route exact path='/dashboard' component={Encounter} />
-          <Route exact path='/signup' component={SignUp} />
-        </Switch>
-      </Router>
-    </Fragment>
+    <Provider store={store}>
+      <Fragment>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/login' component={SignIn} />
+            <Route exact path='/dashboard' component={Encounter} />
+            <Route exact path='/signup' component={SignUp} />
+          </Switch>
+        </Router>
+      </Fragment>
+    </Provider>
   );
 };
 
